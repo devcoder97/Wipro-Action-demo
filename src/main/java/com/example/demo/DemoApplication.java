0eabb7f2437d4af00f9bb3083049eb721e16172e
devcoder97 <142200451+devcoder97@users.noreply.github.com>
2023-08-31 04:57:16 +0000

    Roll back

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("I'm Running and from java 17");
+		System.out.println("I'm Running");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
